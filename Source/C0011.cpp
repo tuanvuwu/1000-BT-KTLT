6d9eec757520d7fe674304ad256a3c3d7cb20a9f
Nguyen Tuan Vu <nguyentuanvu.hufi@gmail.com>
2023-03-09 10:56:12 +0700

    Update

---
+++ Source/C0011.cpp
@@ -0,0 +1,25 @@
+﻿/*
+* Chương 1: Lưu đồ thuật toán
+*Bài 11: Tính S(n) = 1 + 1.2 + 1.2.3 + … + 1.2.3….N
+*/
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	long S = 0;
+	long P = 1;
+	cout << "Nhap n: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		P *= i;
+		S += P;
+	}
+	cout << "S(" << n << ")" << "= " << S;
+	return 1;
+
+
+}
---
+++ Source/C0012.cpp
@@ -0,0 +1,22 @@
+﻿/*
+* Chương 1: Lưu đồ thuật toán
+*Bài 12: Tính S(n) = x + x^2 + x^3 + … + x^n
+*/
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	int n, x;
+	long S=0;
+	cout << "Nhap n: ";
+	cin >> n;
+	cout << "Nhap x: ";
+	cin >> x;
+	for (int i = 1; i <= n; i++)
+		S += pow(x, i);
+	cout << "S(" << n << ")" << "=" << S;
+	return 1;
+}
